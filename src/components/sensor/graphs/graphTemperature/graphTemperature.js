a510f7eb55a7bb4cfcff50d7fc04de3ed5ad3132
wardt7 <wardt7@coventry.ac.uk>
2019-03-19 15:13:32 +0000

    Added modals for sensors ready to accept graphing data.

---
+++ src/components/sensor/graphs/graphTemperature/graphTemperature.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import '../modal.css'
+import './graphTemperature.css'
+
+class GraphTemperature extends Component {
+    constructor(props){
+	super(props)
+	this.handleModalExitClick = this.handleModalExitClick.bind(this)
+	this.handleModalContentClick = this.handleModalContentClick.bind(this)
+    }
+    handleModalExitClick(event){
+	this.props.onModalExitClick()
+    }
+    handleModalContentClick(event){
+	event.stopPropagation()
+	return null
+    }
+    render(){
+	return (
+		<div className="modal" onClick={this.handleModalExitClick}>
+		<div className="modalContent" onClick={this.handleModalContentClick}>
+		<p>Hello world!</p>
+		</div>
+		</div>
+	)
+    }
+}
+
+export default GraphTemperature
