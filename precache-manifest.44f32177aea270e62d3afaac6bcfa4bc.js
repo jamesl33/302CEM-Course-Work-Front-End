50bca7ea85a7e80925760714ef67e07bf0b29143
wardt7 <wardt7@coventry.ac.uk>
2019-03-19 15:24:10 +0000

    Updates

---
+++ precache-manifest.44f32177aea270e62d3afaac6bcfa4bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a8fdcca51c6c1f82254",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/js/runtime~main.8a8fdcca.js"
+  },
+  {
+    "revision": "9d067f6e809ef81d235e",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/js/main.9d067f6e.chunk.js"
+  },
+  {
+    "revision": "5c35c58d8937bea029e6",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/js/2.5c35c58d.chunk.js"
+  },
+  {
+    "revision": "9d067f6e809ef81d235e",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/css/main.21c713e5.chunk.css"
+  },
+  {
+    "revision": "5c35c58d8937bea029e6",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/css/2.27be34b4.chunk.css"
+  },
+  {
+    "revision": "b2dd41a3a27a347e82327c2b9cb0328f",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/index.html"
+  }
+];
