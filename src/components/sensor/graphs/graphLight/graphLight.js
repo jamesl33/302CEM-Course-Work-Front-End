1fa23a66ae8ca8ccf7ca6a9916731def5ce2a85a
wardt7 <wardt7@coventry.ac.uk>
2019-03-20 12:47:57 +0000

    Added graph containers but need to properly configue data use

--- src/components/sensor/graphs/graphLight/graphLight.js
+++ src/components/sensor/graphs/graphLight/graphLight.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Charts, ChartContainer, ChartRow, YAxis, LineChart } from 'react-timeseries-charts'
+import { TimeSeries } from 'pondjs'
 import '../modal.css'
 import './graphLight.css'
 
@@ -16,10 +18,28 @@ class GraphLight extends Component {
 	return null
     }
     render(){
+	const historyRange = new TimeSeries({
+	    name: "Historical Light",
+	    columns: ["time", "light"],
+	    points: [[1553084676, 20000],
+		     [1553084686, 19000],
+		     [1553084696, 18000],
+		     [1553084740, 17000]
+		    ]
+	})
 	return (
 		<div className="modal" onClick={this.handleModalExitClick}>
 		<div className="modalContent" onClick={this.handleModalContentClick}>
-		<p>Hello world!</p>
+		
+		<ChartContainer timeRange={historyRange.range()} format="%b '%y">
+		<ChartRow height="150">
+		<YAxis id="light" label="Light Level" min={historyRange.min()} max={historyRange.max()} width="60" format=".2f"/>
+		<Charts>
+		<LineChart axis="light" series={historyRange} />
+		</Charts>
+		</ChartRow>
+		</ChartContainer>
+		
 		</div>
 		</div>
 	)
--- src/components/sensor/graphs/graphTemperature/graphTemperature.js
+++ src/components/sensor/graphs/graphTemperature/graphTemperature.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Charts, ChartContainer, ChartRow, YAxis, LineChart } from 'react-timeseries-charts'
+import { TimeSeries } from 'pondjs'
 import '../modal.css'
 import './graphTemperature.css'
 
@@ -16,10 +18,28 @@ class GraphTemperature extends Component {
 	return null
     }
     render(){
+	const historyRange = new TimeSeries({
+	    name: "Historical Temperatures",
+	    columns: ["time", "temperature"],
+	    points: [[1553084676, 20.1],
+		     [1553084686, 22.5],
+		     [1553084696, 24.6],
+		     [1553084740, 28.0]
+		    ]
+	})
 	return (
 		<div className="modal" onClick={this.handleModalExitClick}>
 		<div className="modalContent" onClick={this.handleModalContentClick}>
-		<p>Hello world!</p>
+		
+		<ChartContainer timeRange={historyRange.timerange()} format="%b '%y">
+		<ChartRow height="150">
+		<YAxis id="temperature" label="Temperature, C" min={historyRange.min()} max={historyRange.max()} width="60" format=".2f"/>
+		<Charts>
+		<LineChart axis="temperature" series={historyRange} />
+		</Charts>
+		</ChartRow>
+		</ChartContainer>
+	    
 		</div>
 		</div>
 	)
