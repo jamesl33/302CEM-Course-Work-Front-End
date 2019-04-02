7082b25acc35114aa758d11afe47d6b5a160f4a8
James Lee <jamesl33info@gmail.com>
2019-04-02 21:31:12 +0100

    Updates

---
+++ precache-manifest.0389c3ca2a6aa8447ac611b401d047fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e34929540633310dbfb8",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/js/runtime~main.29eb4c1a.js"
+  },
+  {
+    "revision": "b89465768e610ec94978",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/js/main.f51075c3.chunk.js"
+  },
+  {
+    "revision": "ae17442a6674e28cfbab",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/js/2.f736cd3d.chunk.js"
+  },
+  {
+    "revision": "b89465768e610ec94978",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/css/main.b1c73e4d.chunk.css"
+  },
+  {
+    "revision": "ae17442a6674e28cfbab",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/static/css/2.5874396c.chunk.css"
+  },
+  {
+    "revision": "aa961ae563ebf6ed6883cc8495d37c17",
+    "url": "/pages/leej64/Bear-302CEM-Course-Work-Front-End/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pages/leej64/Bear-302CEM-Course-Work-Front-End/precache-manifest.78184eaa037319de97ab56d87f5c4d9e.js"
+  "/pages/leej64/Bear-302CEM-Course-Work-Front-End/precache-manifest.0389c3ca2a6aa8447ac611b401d047fe.js"
 );
 
 workbox.clientsClaim();
